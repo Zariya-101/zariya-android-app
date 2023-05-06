b5249325456b18e0fdb498346ae25ad32d99b82e
Suraj <13surajprakash13@gmail.com>
2023-05-06 17:25:01 +0530

    Comments resolved!

--- app/src/main/java/com/zariya/zariya/payment/presentation/fragment/CheckoutFragment.kt
+++ app/src/main/java/com/zariya/zariya/payment/presentation/fragment/CheckoutFragment.kt
@@ -1,15 +1,13 @@
 package com.zariya.zariya.payment.presentation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.zariya.zariya.R
+import com.zariya.zariya.core.ui.BaseFragment
 import com.zariya.zariya.databinding.FragmentCheckoutBinding
-import com.zariya.zariya.databinding.FragmentSignUpBinding
 
-class CheckoutFragment : Fragment() {
+class CheckoutFragment : BaseFragment() {
 
 
     private lateinit var binding:FragmentCheckoutBinding
