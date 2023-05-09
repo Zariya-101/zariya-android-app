8c640ab63ee908c75a8c8678229a39bfefab68bd
Vivek <vivek.sharma@mpokket.com>
2023-05-09 18:18:31 +0530

    Firebase Added

---
+++ app/src/main/java/com/zariya/zariya/core/ui/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.zariya.zariya.core.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+abstract class BaseViewModel(app: Application): AndroidViewModel(app)
