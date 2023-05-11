477352a0a57be0ccf3b56041db172277a932d2c7
Suraj <13surajprakash13@gmail.com>
2023-05-11 18:25:17 +0530

    CartFragment Done!

--- app/src/main/java/com/zariya/zariya/payment/presentation/fragment/MyCartFragment.kt
+++ app/src/main/java/com/zariya/zariya/payment/presentation/fragment/MyCartFragment.kt
@@ -1,22 +1,24 @@
 package com.zariya.zariya.payment.presentation.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.zariya.zariya.R
 import com.zariya.zariya.core.ui.BaseFragment
-import com.zariya.zariya.databinding.FragmentCheckoutBinding
+import com.zariya.zariya.databinding.FragmentMyCartBinding
 
-class CheckoutFragment : BaseFragment() {
+class MyCartFragment :  BaseFragment() {
 
 
-    private lateinit var binding:FragmentCheckoutBinding
+    private lateinit var binding: FragmentMyCartBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCheckoutBinding.inflate(layoutInflater)
+        binding = FragmentMyCartBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -26,13 +28,10 @@ class CheckoutFragment : BaseFragment() {
         activity?.setActionBar(binding.toolbar)
         activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
         activity?.actionBar?.setDisplayShowHomeEnabled(true)
-        activity?.actionBar?.title = "Almost there!"
+        activity?.actionBar?.title = "My Cart"
         binding.toolbar.setNavigationOnClickListener {
             Navigation.findNavController(it).popBackStack()
-
         }
+
     }
-
 }
-
-
