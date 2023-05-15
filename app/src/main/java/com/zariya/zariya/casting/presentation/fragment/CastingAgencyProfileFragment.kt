f57793ec16aff14e511758c8b7eb9ff0b80a153d
Suraj <13surajprakash13@gmail.com>
2023-05-15 16:25:07 +0530

    Casting Agency Comments Resolved!

--- app/src/main/java/com/zariya/zariya/casting/presentation/fragment/CastingAgencyProfileFragment.kt
+++ app/src/main/java/com/zariya/zariya/casting/presentation/fragment/CastingAgencyProfileFragment.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.tabs.TabLayout
+import com.zariya.zariya.R
 import com.zariya.zariya.core.ui.BaseFragment
 import com.zariya.zariya.databinding.FragmentCastingAgencyProfileBinding
 
 class CastingAgencyProfileFragment : BaseFragment() {
 
-    var tabLayout: TabLayout? = null
     private lateinit var binding: FragmentCastingAgencyProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +22,8 @@ class CastingAgencyProfileFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tabLayout = binding.tabLayout
-
-        tabLayout!!.addTab(tabLayout!!.newTab().setText("About"))
-        tabLayout!!.addTab(tabLayout!!.newTab().setText("Casting Calls"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(getString(R.string.about)))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(getString(R.string.casting_calls)))
 
     }
 }
