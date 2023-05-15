182db8f83756f32db919b7423b7fd5bfdc9419a3
Suraj <13surajprakash13@gmail.com>
2023-05-15 10:26:32 +0530

    Casting Agency Profile Done!

--- app/src/main/java/com/zariya/zariya/casting/presentation/fragment/CastingAgencyProfileFragment.kt
+++ app/src/main/java/com/zariya/zariya/casting/presentation/fragment/CastingAgencyProfileFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayout
 import com.zariya.zariya.core.ui.BaseFragment
 import com.zariya.zariya.databinding.FragmentCastingAgencyProfileBinding
 
 class CastingAgencyProfileFragment : BaseFragment() {
 
+    var tabLayout: TabLayout? = null
     private lateinit var binding: FragmentCastingAgencyProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +22,11 @@ class CastingAgencyProfileFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tabLayout = binding.tabLayout
+
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("About"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Casting Calls"))
 
     }
 }
