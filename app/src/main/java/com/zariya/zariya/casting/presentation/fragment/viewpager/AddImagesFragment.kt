967338f2ded0e316315e8d41e87aa7e969503dcc
Vivek <vivek.sharma@mpokket.com>
2023-05-18 17:31:57 +0530

    Actor Profile Page added

--- app/src/main/java/com/zariya/zariya/casting/presentation/fragment/viewpager/AddImagesFragment.kt
+++ app/src/main/java/com/zariya/zariya/casting/presentation/fragment/viewpager/AddImagesFragment.kt
@@ -1,17 +1,36 @@
 package com.zariya.zariya.casting.presentation.fragment.viewpager
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.zariya.zariya.casting.presentation.adapter.AddImageAdapter
 import com.zariya.zariya.casting.presentation.viewmodel.CastingOnboardingViewModel
 import com.zariya.zariya.core.ui.BaseFragment
 import com.zariya.zariya.databinding.FragmentAddImagesBinding
+
 
 class AddImagesFragment(private val castingOnboardingViewModel: CastingOnboardingViewModel) :
     BaseFragment() {
 
     private lateinit var binding: FragmentAddImagesBinding
+
+    private val PERMISSION_READ_MEDIA_IMAGES = 1
+    private val PERMISSION_WRITE_EXTERNAL = 1
+
+    private var imagesList: ArrayList<String?> = arrayListOf("")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +38,75 @@ class AddImagesFragment(private val castingOnboardingViewModel: CastingOnboardin
     ): View {
         binding = FragmentAddImagesBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvImages.apply {
+            adapter = AddImageAdapter(imagesList,
+                onItemClick = {
+                    checkGalleryPermission()
+                },
+                onCloseClick = {
+                    imagesList.remove(it)
+                    binding.rvImages.adapter?.notifyDataSetChanged()
+                })
+        }
+    }
+
+    private fun checkGalleryPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                } != PackageManager.PERMISSION_GRANTED
+            ) {
+                activity?.let {
+                    ActivityCompat.requestPermissions(
+                        it,
+                        arrayOf<String>(Manifest.permission.READ_MEDIA_IMAGES),
+                        PERMISSION_READ_MEDIA_IMAGES
+                    )
+                }
+            } else {
+                openGallery()
+            }
+        } else {
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                } != PackageManager.PERMISSION_GRANTED
+            ) {
+                activity?.let {
+                    ActivityCompat.requestPermissions(
+                        it,
+                        arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        PERMISSION_WRITE_EXTERNAL
+                    )
+                }
+            } else {
+                openGallery()
+            }
+        }
+    }
+
+    var galleryActivityResultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val uri = result.data?.data
+            imagesList.add(0, uri.toString())
+            binding.rvImages.adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryActivityResultLauncher.launch(galleryIntent)
     }
 }
